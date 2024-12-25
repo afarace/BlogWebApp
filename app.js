0365f14dfbe05fe9f6ffa2c804f1e47d4449e4f1
Agustin Farace <agustin.farace@mi.unc.edu.ar>
2024-12-25 15:54:20 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+const blogs = [];
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/create", (req, res) => {
+  res.render("create.ejs");
+});
+
+app.post("/create", (req, res) => {
+  const date = new Date().toDateString();
+  blogs.push({
+    day: date,
+    author: req.body["author"],
+    title: req.body["title"],
+    body: req.body["body"],
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
