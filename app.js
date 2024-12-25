28dd50f4208c5c964f86ef51f0998cde9a9900ff
Agustin Farace <agustin.farace@mi.unc.edu.ar>
2024-12-25 18:44:43 -0300

    Add functionality Read a blog

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 const blogs = [];
+var blogID = 0;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,14 +22,21 @@ app.get("/view", (req, res) => {
   res.render("view.ejs", { blogs });
 });
 
+app.get("/view/:id", (req, res) => {
+  const blog = blogs.find(item => item.id === parseInt(req.params.id));
+  res.render("blog.ejs", { blog });  
+});
+
 app.post("/create", (req, res) => {
   const date = new Date().toDateString();
   blogs.push({
+    id: blogID,
     day: date,
     author: req.body["author"],
     title: req.body["title"],
     body: req.body["body"],
   });
+  blogID++
 });
 
 app.listen(port, () => {
